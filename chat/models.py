916d696b270e24c677f007f93bfaadba2e3629fd
SnehaPrasad23 <snehaprasad.in@gmail.com>
2024-12-27 00:59:59 +0530

    modified model

--- chat/models.py
+++ chat/models.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 # Create your models here.
 class Room(models.Model):
-    name = models.CharField(max_length=1000)
+    name = models.CharField(max_length=100)
 
 class Message(models.Model):
-    value = models.CharField(max_length=100000)
+    value = models.TextField(max_length=10000)
     date = models.DateTimeField(default=datetime.now, blank=True)
-    user = models.CharField(max_length=100000)
-    room = models.CharField(max_length=100000)
+    user = models.CharField(max_length=100)
+    room = models.CharField(max_length=100)
